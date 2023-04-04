f2fe680d2c89ff6a72e0fb6f8cff477207bc54e1
Akas_Datta <012akasdatta@gmail.com>
2023-04-04 15:47:18 +0600

    json data modified and add that

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const PriceCard = ({price}) => {
     return (
         <div>
-            {price.name}
+            <h2>
+                <span className='text-purple-700 text-5xl font-extrabold'>
+                    {price.price}
+                </span>
+                <span className='text-2xl'>/mon</span>
+            </h2>
+            <h5 className='text-2xl font-bold'>{price.name}</h5>
         </div>
     );
 };
--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -13,12 +13,14 @@ const PriceList = () => {
     return (
         <div>
             <h2 className='text-5xl text-center text-red-600 bg-purple-300 font-bold p-4 '>Awesome Affordable</h2>
+           <div className='grid grid-cols-3 gap-3'>
             {
-                prices.map(price => <PriceCard
-                    key={price.id}
-                    price={price}>
-                </PriceCard>)
-            }
+                    prices.map(price => <PriceCard
+                        key={price.id}
+                        price={price}>
+                    </PriceCard>)
+                }
+           </div>
         </div>
     );
 };
