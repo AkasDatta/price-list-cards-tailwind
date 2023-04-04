fd678ea224c39dda2f7a8f1273fdc5ac9d9ea704
Akas_Datta <012akasdatta@gmail.com>
2023-04-04 14:41:24 +0600

    add json data and create pricelist and pricecard

--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PriceCard from '../PriceCard/PriceCard';
 
 const PriceList = () => {
+    const [prices, setPrices] = useState([]);
+
+    useEffect(() => {
+        fetch('prices.json')
+        .then(res => res.json())
+        .then(data => setPrices(data));
+    }, [])
+
     return (
         <div>
-            <h2>Awesome Affordable</h2>
+            <h2 className='text-5xl text-center text-red-600 bg-purple-300 font-bold p-4 '>Awesome Affordable</h2>
+            {
+                prices.map(price => <PriceCard
+                    key={price.id}
+                    price={price}>
+                </PriceCard>)
+            }
         </div>
     );
 };
